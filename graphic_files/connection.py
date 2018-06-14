31f2e05ad27495ca47b3fdae3416fac5ccd2eb31
quentin.payre <quentin.payre@epitech.eu>
2018-06-14 17:43:13 +0100

    add : struct of fct that sort infos before display on map (food/stones)

--- graphic_files/connection.py
+++ graphic_files/connection.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 import sys, pygame, socket
 from pygame.locals import *
+from map_handler.py
 
 #_______________________________________________________________________________
 # CONNECTION SERVER
 def connect_client(port, host):
-	str1 = "WELCOME"
+	welcome = "WELCOME"
 	str2 = "mbape"
 	sockett = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sockett.connect((host, port))
 	print("Connected : {}".format(port))
 	data = sockett.recv(255)
-	if str(data).find(str1) == 2:
+	if str(data).find(welcome) == 2:
 		sockett.send("GMAP\n".encode())
 	data = sockett.recv(255)
 	#if str(data).find(str2) == 2:
-		#handle_map()
+		#handle_map(sockett)
 	sockett.close()
-
-#_______________________________________________________________________________
-# GESTION CLIENT
-def get_port(arg1):
-	port = int(arg1)
-	if port < 0 or port > 65535:
-		return (-1)
-	return (port)
---
+++ graphic_files/handle_player.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+import sys
+
+def create_handle(data):
+	print("handle players")
+	print(data)
