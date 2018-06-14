65638f11cce612c78a5bae049d40046de7d7b630
nicolas cova <nicolas.cova@epitech.eu>
2018-06-14 14:35:08 +0200

    découpage du fichiers principales en plusieurs + ajout des textures

---
+++ graphic_files/map_handler.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import sys, pygame, socket
+from pygame.locals import *
+
+#a enlever apres que le pb de mbmap soit regle cote server
+SizeX = 10
+SizeY = 10
+ 
+# SIZE OF A BOX BY DEFAULT
+size_box = 100
+ 
+#_______________________________________________________________________________
+# DATA MAP
+def handle_map():
+  if (SizeX >= 100 and SizeX < 1000 or
+     SizeY >= 100 and SizeY < 1000):
+    width = SizeX * (size_box / 10)
+    height = SizeY * (size_box / 10)
+  elif (SizeX >= 1000 and SizeX < 10000 or
+     SizeY >= 1000 and SizeY < 10000):
+    width = SizeX * (size_box / 100)
+    height = SizeY * (size_box / 100)
+  elif (SizeX >= 10000 or SizeY >= 10000):
+    width = SizeX * (size_box / 1000)
+    height = SizeY * (size_box / 1000)
+  else:
+    width = SizeX * size_box
+    height = SizeY * size_box
+  create_window(int(width), int(height))
+ 
+def load_tab_info():
+  line = SizeX
+  column = SizeY
+  lst = [[0] * column for _ in range(line)]
