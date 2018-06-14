95e41846f317b0f99cf23b2a8334f530ce9c7682
quentin.payre <quentin.payre@epitech.eu>
2018-06-14 14:42:08 +0100

    update: coding style new files

--- graphic_files/connection.py
+++ graphic_files/connection.py
@@ -5,23 +5,23 @@ from pygame.locals import *
 #_______________________________________________________________________________
 # CONNECTION SERVER
 def connect_client(port, host):
-  str1 = "WELCOME"
-  str2 = "mbape"
-  sockett = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sockett.connect((host, port))
-  print("Connected : {}".format(port))
-  data = sockett.recv(255)
-  if str(data).find(str1) == 2:
-    sockett.send("GMAP\n".encode())
-  data = sockett.recv(255)
-  #if str(data).find(str2) == 2:
-  #  handle_map()
-  sockett.close()
-   
+	str1 = "WELCOME"
+	str2 = "mbape"
+	sockett = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sockett.connect((host, port))
+	print("Connected : {}".format(port))
+	data = sockett.recv(255)
+	if str(data).find(str1) == 2:
+		sockett.send("GMAP\n".encode())
+	data = sockett.recv(255)
+	#if str(data).find(str2) == 2:
+		#handle_map()
+	sockett.close()
+
 #_______________________________________________________________________________
 # GESTION CLIENT
 def get_port(arg1):
-  port = int(arg1)
-  if port < 0 or port > 65535:
-    return (-1)
-  return (port)
+	port = int(arg1)
+	if port < 0 or port > 65535:
+		return (-1)
+	return (port)
