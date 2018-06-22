9d80779ff803dff1b0be52ab32ad11bc35fb30d6
nicolas cova <nicolas.cova@epitech.eu>
2018-06-22 16:17:26 +0200

    displaying of animation player and rocks/food

---
+++ graphic_files/cha_moving.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import sys, pygame
+from pygame.locals import *
+
+#_______________________________________________________________________________
+# MAKE THE ANIMATION OF FORWARD 
+def forward (sprite, tmp, check):
+	if check == 1:
+		Img = pygame.image.load('../textures/character4.1.png')
+		fenetre.blit(Img, ( x+10, y))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character4.2.png')
+		fenetre.blit(Img, (x+20, y))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character4.png')
+		if (x > 640):
+			x = x - 640
+		else:
+			x = x+20
+		fenetre.blit(Img, ( x,y))
+		pygame.display.flip()
+
+
+	elif check == 2:
+		Img = pygame.image.load('../textures/character2.1.png')
+		fenetre.blit(Img, ( x-10, y))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character2.2.png')
+		fenetre.blit(Img, (x-20, y))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character2.png')
+		if (x < 0):
+			x =  640 + x
+		else:
+			x = x-20
+		fenetre.blit(Img, ( x,y))
+		pygame.display.flip()
+
+
+	elif check == 3:
+		Img = pygame.image.load('../textures/character1.1.png')
+		fenetre.blit(Img, ( x, y+10))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character1.2.png')
+		fenetre.blit(Img, (x, y+20))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character1.png')
+		if (y > 480):
+			y = y - 480
+		else:
+			y = y+20
+		fenetre.blit(Img, ( x,y))
+		pygame.display.flip()
+
+
+	elif check == 4:
+		Img = pygame.image.load('../textures/character3.1.png')
+		fenetre.blit(Img, ( x, y-10))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character3.2.png')fenetre.blit(Img, (x, y-20))
+		pygame.display.flip()
+		time.sleep(0.15)
+		Img = pygame.image.load('../textures/character3.png')
+		if (y < 480):
+			y = 480 + y
+		else:
+			y = y-20
+		fenetre.blit(Img, ( x,y))
+		pygame.display.flip()
