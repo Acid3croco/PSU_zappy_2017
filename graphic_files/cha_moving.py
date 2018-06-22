aa8011e4f0f5e05e90c28c4231de4a8c71db7bc7
nicolas cova <nicolas.cova@epitech.eu>
2018-06-22 16:37:17 +0200

    Player can change direction RIGHT or LEFT

--- graphic_files/cha_moving.py
+++ graphic_files/cha_moving.py
@@ -3,19 +3,36 @@
 import sys, pygame
 from pygame.locals import *
 
-#_______________________________________________________________________________
+#
+_______________________________________________________________________________
+# CHANGE DIRECTION OF CHARACTER
+def right_left(Img, check, r_l):
+	tmp = "./textures/character"
+	if check == 1 and r_l == 0:
+		tmp += str(4)
+	elif check == 4 and r_l == 1:
+		tmp += str(1)
+	elif r_l == 0:
+		tmp += str(check - 1)
+	elif r_l == 1:
+		tmp += str(check + 1)
+	tmp += ".png"
+	Img = pygame.image.load(tmp)
+
+#
+_______________________________________________________________________________
 # MAKE THE ANIMATION OF FORWARD 
-def forward (sprite, tmp, check):
+def forward (Img, check):
 	if check == 1:
-		Img = pygame.image.load('../textures/character4.1.png')
+		Img = pygame.image.load('./textures/character4.1.png')
 		fenetre.blit(Img, ( x+10, y))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character4.2.png')
+		Img = pygame.image.load('./textures/character4.2.png')
 		fenetre.blit(Img, (x+20, y))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character4.png')
+		Img = pygame.image.load('./textures/character4.png')
 		if (x > 640):
 			x = x - 640
 		else:
@@ -25,15 +42,15 @@ def forward (sprite, tmp, check):
 
 
 	elif check == 2:
-		Img = pygame.image.load('../textures/character2.1.png')
+		Img = pygame.image.load('./textures/character2.1.png')
 		fenetre.blit(Img, ( x-10, y))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character2.2.png')
+		Img = pygame.image.load('./textures/character2.2.png')
 		fenetre.blit(Img, (x-20, y))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character2.png')
+		Img = pygame.image.load('./textures/character2.png')
 		if (x < 0):
 			x =  640 + x
 		else:
@@ -43,15 +60,15 @@ def forward (sprite, tmp, check):
 
 
 	elif check == 3:
-		Img = pygame.image.load('../textures/character1.1.png')
+		Img = pygame.image.load('./textures/character1.1.png')
 		fenetre.blit(Img, ( x, y+10))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character1.2.png')
+		Img = pygame.image.load('./textures/character1.2.png')
 		fenetre.blit(Img, (x, y+20))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character1.png')
+		Img = pygame.image.load('./textures/character1.png')
 		if (y > 480):
 			y = y - 480
 		else:
@@ -61,14 +78,14 @@ def forward (sprite, tmp, check):
 
 
 	elif check == 4:
-		Img = pygame.image.load('../textures/character3.1.png')
+		Img = pygame.image.load('./textures/character3.1.png')
 		fenetre.blit(Img, ( x, y-10))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character3.2.png')fenetre.blit(Img, (x, y-20))
+		Img = pygame.image.load('./textures/character3.2.png')fenetre.blit(Img, (x, y-20))
 		pygame.display.flip()
 		time.sleep(0.15)
-		Img = pygame.image.load('../textures/character3.png')
+		Img = pygame.image.load('./textures/character3.png')
 		if (y < 480):
 			y = 480 + y
 		else:
--- graphic_files/test/test.py
+++ graphic_files/test/test.py
@@ -11,9 +11,40 @@ WHITE = 255, 255, 255
 GREEN = 0, 255, 0
 RED = 255, 0, 0
 
+def test_right(Img, fenetre):
+    tmp == "RIGHT"
+    if tmp == "RIGHT":
+        print("ok")
+        Img = pygame.image.load('../textures/character4.1.png')
+        fenetre.blit(Img, ( x+10, y))
+        pygame.display.flip()
+        time.sleep(0.15)
+        Img = pygame.image.load('../textures/character4.2.png')
+        fenetre.blit(Img, (x+20, y))
+        pygame.display.flip()
+        time.sleep(0.15)
+        Img = pygame.image.load('../textures/character4.png')
+        if (x > 640):
+            x = x - 640
+        else:
+            x = x+20
+        fenetre.blit(Img, ( x,y))
+        pygame.display.flip()
+
+def choose_rock(x_square, y_square,  rock, pos, fenetre):
+    all_rocks = {}
+    all_rocks["linemate"] = pygame.image.load('../textures/linemate.png')
+    all_rocks["dereaumere"] = pygame.image.load('../textures/deraumere.png')
+    all_rocks["sibur"] = pygame.image.load('../textures/sibur.png')
+    all_rocks["mendiane"] = pygame.image.load('../textures/Mendiane.png')
+    all_rocks["phiras"] = pygame.image.load('../textures/phirras.png')
+    all_rocks["thystmane"] = pygame.image.load('../textures/thystame.png')
+    if rock == "L":
+        fenetre.blit(all_rocks["linemate"], ((x_square / 2) * pos, (y_square /2) * pos) )
+    pygame.display.flip()
 
 def main():
-    fenetre = pygame.display.set_mode((640, 480))
+    fenetre = pygame.display.set_mode((1200, 720))
     loop = True
     green_color = GREEN
     white_color = WHITE
@@ -21,9 +52,18 @@ def main():
     height=200
     x = 40
     y = 80
+    tmp = 2
     Img = pygame.image.load('../textures/character4.png')
-    Food = pygame.image.load('../textures/food.png')
-
+    all_rocks = {}
+    all_rocks["linemate"] = pygame.image.load('../textures/linemate.png')
+    all_rocks["dereaumere"] = pygame.image.load('../textures/deraumere.png')
+    all_rocks["sibur"] = pygame.image.load('../textures/sibur.png')
+    all_rocks["mendiane"] = pygame.image.load('../textures/Mendiane.png')
+    all_rocks["phiras"] = pygame.image.load('../textures/phirras.png')
+    all_rocks["thystmane"] = pygame.image.load('../textures/thystame.png') 
+    all_rocks["Food"] = pygame.image.load('../textures/food.png')
+    Img = pygame.transform.scale(Img, (100,100))
+    choose_rock(100,  50, "L", 5, fenetre)
     while loop:
         background = pygame.Surface(fenetre.get_size())
         background.fill(CIEL)
@@ -31,8 +71,8 @@ def main():
         # Ajout du fond dans la fenêtre
         fenetre.blit(background, (0, 0))
         fenetre.blit(Img, ( x,y))
-        fenetre.blit(Food, (300, 300))
-        pygame.display.flip()
+        fenetre.blit(all_rocks["Food"], (300, 300))
+        choose_rock(100,  50, "L", 10, fenetre)
 
         # Draw a rectangle outline
         rect_white = pygame.draw.rect(fenetre, white_color, [75, 10, 100, 50],
@@ -48,9 +88,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
-
-            # si clic, le vert devient rouge
+            elif tmp == 2:
+                print("ok")
+                Img = pygame.image.load('../textures/character4.1.png')
+                fenetre.blit(Img, ( x+10, y))
+                pygame.display.flip()
+                time.sleep(0.15)
+                Img = pygame.image.load('../textures/character4.2.png')
+                fenetre.blit(Img, (x+20, y))
+                pygame.display.flip()
+                time.sleep(0.15)
+                Img = pygame.image.load('../textures/character4.png')
+                if (x > 640):
+                    x = x - 640
+                else:
+                    x = x+20
+                fenetre.blit(Img, ( x,y))
+                pygame.display.flip()
             elif event.type == pygame.KEYDOWN:
+                
                 if event.key == pygame.K_RIGHT:
                     Img = pygame.image.load('../textures/character4.1.png')
                     fenetre.blit(Img, ( x+10, y))
@@ -123,9 +179,12 @@ def main():
                     fenetre.blit(Img, ( x,y))
                     pygame.display.flip()
             # le rectangle se cache
-            elif event.type == pygame.MOUSEBUTTONDOWN and over_white:
-                white_color = RED
-                Food = pygame.image.load('../textures/deraumere.png')
+                elif event.key == pygame.K_UP:
+                    print("ok")
+                    all_rocks()
+                elif event.type == pygame.MOUSEBUTTONDOWN and over_white:
+                    white_color = RED
+                    Food = pygame.transform.scale(Food, (100,50))
 
         # Actualisation de l'affichage
         pygame.display.flip()
