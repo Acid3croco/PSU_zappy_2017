508b42c17425d1831162de4c49334ba57c820a58
Pierre <pierre.leriche@epitech.eu>
2018-06-23 17:10:24 +0200

    add : -help

--- client_files/src/Client.cpp
+++ client_files/src/Client.cpp
@@ -19,11 +19,20 @@ int	main(int ac, char **av)
 {
 	Game		ga;
 
-	if (ac != 5 && ac != 7)
-		return (84);
-	if (ga.prepGame(ac, av) == false)
-		return (84);
-	if (ga.gameLoop() == false)
-		return (84);
+	if (ac == 2 && std::string(av[1]).compare("-help") == 0) {
+		std::cout << "USAGE:\t./zappy_ai -p port -n name -h machine\n";
+		std::cout << "\tport\tis the port number\n\tname\t";
+		std::cout << "is the name of the team\n\tmachine\t";
+		std::cout << "is the name of the machine;";
+		std::cout << "localhost by default" << std::endl;
+	}
+	else {
+		if (ac != 5 && ac != 7)
+			return (84);
+		if (ga.prepGame(ac, av) == false)
+			return (84);
+		if (ga.gameLoop() == false)
+			return (84);
+	}
 	return (0);
 }
